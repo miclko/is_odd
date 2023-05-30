cab687c88a9ad0aef219473aa5d98f1d761f5294
miclko <michael.konopik@ftf.lth.se>
2023-05-30 14:19:27 +0200

    Add 'is_odd' function and documentation

--- is_odd.py
+++ is_odd.py
@@ -1,8 +1,17 @@
 from is_even import is_even
 
-
+def is_odd(x):
+    """ Is odd function checks for odd integers, using even function
+    x: integer
+    output:
+    true if x is odd
+    false if x is even
+    Exception if no integer
+    """
 
 
 
 
 assert is_even is not None
+
+assert is_odd.__doc__ is not None
